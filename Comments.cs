2bf22606000c3aaf1b71d0f4ddd97635d5739ad4
bhavishyapothula <bhavishya.pothula@gmail.com>
2020-01-22 12:48:46 +0530

    Update Comments.cs

--- Comments.cs
+++ Comments.cs
@@ -7,21 +7,23 @@ namespace Salary
         static void Main(string[] args)
         {
             Console.WriteLine("Enter basic salary");
+            //Reads basic salary from console
             double bs = Convert.ToDouble(Console.ReadLine());
             CalculateSalary c = new CalculateSalary();
             Console.WriteLine("Net salary:"+c.CalculateNet(bs));
         }
     }
     /// <summary>
-    /// This class contains methods to calculate gross salary
+    /// This class contains methods to calculate net salary
     /// </summary>
     /// <remarks>This class contains methods to calculate da,hra,pf and net salary</remarks>
     class CalculateSalary
-    {/// <summary>
-    /// This method calculates DA for given basic salary 
-    /// </summary>
-    /// <param name="salary">This method takes double value as parameter</param>
-    /// <returns>Returns double value</returns>
+    {
+        /// <summary>
+        /// This method calculates DA for given basic salary 
+        /// </summary>
+        /// <param name="salary">This method takes double value as parameter</param>
+        /// <returns>Returns double value</returns>
         double CalculateDA(double salary)
         {
             return salary * 0.3;
